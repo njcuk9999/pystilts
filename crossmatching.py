e2c7ccd8b33d2e071ccc1c82efd2dea079d82c60
Neil James Cook <neil.james.cook@gmail.com>
2017-07-10 18:14:56 -0400

    Updated tmatch2 comments

--- crossmatching.py
+++ crossmatching.py
@@ -61,6 +61,111 @@ def tapskymatch(**kwargs):
 
 
 def tmatch2(**kwargs):
+    """
+    keywords are:
+    
+    in1         string, The location of the first input table. This may take 
+                one of the following forms: 
+                
+                - A filename.
+                - A URL.
+    
+    in2         string, The location of the second input table. This may take 
+                one of the following forms: 
+                
+                - A filename.
+                - A URL.
+                           
+    matcher     string, Defines the nature of the matching that will be 
+                performed. Depending on the name supplied, this may be 
+                positional matching using celestial or Cartesian coordinates, 
+                exact matching on the value of a string column, or other things.
+                 
+                must be one of the following:
+                
+                - sky: The sky matcher compares positions on the celestial 
+                       sphere with a fixed error radius. Rows are considered 
+                       to match when the two (ra, dec) positions are within 
+                       max-error arcseconds of each other along a great circle. 
+
+                    values: 
+                        ra/degrees: Right Ascension 
+                        dec/degrees: Declination
+                    params:
+                        max-error/arcsec: Maximum separation along a great circle
+
+                - skyerr
+                - skyellipse
+                - sky3d
+                - exact
+                - 1d, 2d, ...
+                - 2d_anisotropic, ...
+                - 2d_cuboid, ...
+                - 1d_err, 2d_err, ...
+                - 2d_ellipse
+                
+                this changes the values that need to be set
+                
+    values1     string, Defines the values from table 1 which are used to 
+                determine whether a match has occurred. These will typically 
+                be coordinate values such as RA and Dec and perhaps some 
+                per-row error values as well, though exactly what values are 
+                required is determined by the kind of match as determined by 
+                matcher.
+                
+    value2      string, Defines the values from table 2 which are used to 
+                determine whether a match has occurred. These will typically 
+                be coordinate values such as RA and Dec and perhaps some 
+                per-row error values as well, though exactly what values are 
+                required is determined by the kind of match as determined by 
+                matcher.
+                
+    join        string, Determines which rows are included in the output table. 
+                The matching algorithm determines which of the rows from the 
+                first table correspond to which rows from the second. 
+                This parameter determines what to do with that information. 
+                Perhaps the most obvious thing is to write out a table 
+                containing only rows which correspond to a row in both of the 
+                two input tables. However, you may also want to see the 
+                unmatched rows from one or both input tables, or rows present 
+                in one table but unmatched in the other, or other possibilities.
+                The options are:
+
+                    1and2: An output row for each row represented in both input 
+                            tables (INNER JOIN)
+                    
+                    1or2: An output row for each row represented in either or 
+                            both of the input tables (FULL OUTER JOIN)
+                    
+                    all1: An output row for each matched or unmatched row in 
+                            table 1 (LEFT OUTER JOIN)
+                    
+                    all2: An output row for each matched or unmatched row in 
+                            table 2 (RIGHT OUTER JOIN)
+                    
+                    1not2: An output row only for rows which appear in the 
+                            first table but are not matched in the second table
+                    
+                    2not1: An output row only for rows which appear in the 
+                            second table but are not matched in the first table
+                    
+                    1xor2: An output row only for rows represented in one of 
+                            the input tables but not the other one
+
+    icmd1
+    icmd2
+    ocmd
+    params      string, Fixed value(s) giving the parameters of the match 
+                (typically an error radius). If more than one value is required, 
+                the values should be separated by spaces. 
+    out
+    fixcols
+    suffix1
+    suffix2
+    
+    :param kwargs: 
+    :return: 
+    """
     # define command
     command = STILTS
     command += ' tmatch2 '
